f9c36c9accb6900d4f70cfb1d0a403c6c1e739ea
David García Labarga <davgarlab@gmail.com>
2017-03-06 12:13:38 +0100

    Commit02

--- Acceso.java
+++ Acceso.java
@@ -5,19 +5,24 @@ public class Acceso
     private int dia;
     private int hora;
     private int minutos;
-    
+    private String ip;
+    private String web;
+        
     public Acceso(String entradaLog)
     {
         
+       entradaLog = entradaLog.replace("[","");
+       entradaLog = entradaLog.replace("]","");
         
-        String entradasLog[] = entradaLog.split(" ");
+       String entradasLog[] = entradaLog.split(" ");
         
-        
-        this.ano = Integer.parseInt(entradasLog[0]);
-        this.mes = Integer.parseInt(entradasLog[1]);
-        this.dia = Integer.parseInt(entradasLog[2]);
-        this.hora = Integer.parseInt(entradasLog[3]);
-        this.minutos = Integer.parseInt(entradasLog[4]);
+       ip = entradasLog[0];  
+       ano = Integer.parseInt(entradasLog[1]);
+       mes = Integer.parseInt(entradasLog[2]);
+       dia = Integer.parseInt(entradasLog[3]);
+       hora = Integer.parseInt(entradasLog[4]);
+       minutos = Integer.parseInt(entradasLog[5]);
+       web = entradasLog[6];
     }
     
     public int getAno() 
